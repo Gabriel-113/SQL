bbe544beb1c691696c997f9352d9bff81a2ee0de
Gabriel-113 <gabrymenchaca@gmail.com>
2023-02-15 22:08:44 -0300

    Se corrigieron los ejemplos del Stored Procedures

--- Stored_Procedures.sql
+++ Stored_Procedures.sql
@@ -3,9 +3,8 @@ use db_mmorpg;
 -- Este proceso de almacenamiento sirve para visualizar la composicion del servidor de manera detallada y ordenada
 -- de mayor a menor o viceversa correspondiendo al nivel de la zona. Toma dos paramentros, el primero "servidor" (INT) es el numero
 -- correspondiende al id_servidor siendo del 1 al 6; el segundo parametro "nivel" (VARCHAR(5)) acepta 'ASC' y 'DESC' 
--- tomando el camino correspondiente del ordenamiento mediante un condicional if. EJ: CALL INFORMACION_SERVIDOR(3,'ASC').
--- Ej: si el item existe ('Tomo Necronomicon', 'Una Mano', 2500, 0, 80, 'Velocidad cast %50 Mana %40', 'Legendario', '%3')
--- Ej: si el item no existe ('Tomo Sagrado', 'Una Mano', 0, 800, 70, 'Velocidad cast %30 Mana %20', 'Mitico', '%10')
+-- tomando el camino correspondiente del ordenamiento mediante un condicional if. EJ: CALL INFORMACION_SERVIDOR(3,'ASC'), devuelve
+-- la composicion del servidor ordenadas de manera ascendente conforme el nivel de la zona.
 
 drop procedure if exists INFORMACION_SERVIDOR;
 
@@ -52,6 +51,8 @@ DELIMITER ;
 -- se realiza la comprobacion mediante el nombre, en caso de existir el item,
 -- se  devuelve el nombre del item seguido de un mensaje que indica que el item ya existe
 -- si la comprobacion no encuentra el nombre del item en la tabla, inserta el nuevo registro.
+-- Ej: si el item existe ('Tomo Necronomicon', 'Una Mano', 2500, 0, 80, 'Velocidad cast %50 Mana %40', 'Legendario', '%3')
+-- Ej: si el item no existe ('Tomo Sagrado', 'Una Mano', 0, 800, 70, 'Velocidad cast %30 Mana %20', 'Mitico', '%10')
 
 drop procedure if exists INSERCION_ITEM;
 
