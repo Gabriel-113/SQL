e06c8a0eb004a29ad7e8823ab5c6938d4294996e
Gabriel-113 <gabrymenchaca@gmail.com>
2023-02-24 09:38:35 -0300

    Se añadieron comentarios en los scripts

--- Tablas_Log.sql
+++ Tablas_Log.sql
@@ -1,12 +1,16 @@
 use db_mmorpg;
 
+
+// Tabla que registra los movimientos realizados en las tablas USUARIO Y DATOS_USUARIO
+// Interactua con los Triggers REGISTROS (USUARIO), INSERCION_DATOS (DATOS_USUARIO) y ACTUALIZACION_DATOS (ACTUALIZACION_DATOS),
 CREATE TABLE REGISTROS_USUARIOS (
 id_registro INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 movimiento VARCHAR(70),
 fecha_movimiento DATETIME NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-
+// Tabla que registra los movimientos en la tabla BANS
+// Interactua con los Triggers SANCION (BANS) y FINALIZACION_SANCION (BANS)
 CREATE TABLE SANCIONES (
 id_sancion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 movimiento VARCHAR(200),
--- Triggers.sql
+++ Triggers.sql
@@ -1,6 +1,7 @@
 use db_mmorpg;
 
-
+// Trigger que despues de que se realiza una insercion en la tabla USUARIO, inserta un comentario en la tabla REGISTROS_USUARIOS
+// Registrando cada nuevo usuario
 DELIMITER //
 CREATE TRIGGER REGISTROS AFTER INSERT ON USUARIO
 FOR EACH ROW 
@@ -9,7 +10,7 @@ insert into REGISTROS_USUARIOS (movimiento) value (concat('Nuevo usuario registr
 END // 
 DELIMITER ;
 
-
+// Trigger que registra cuando se insertan datos en la tabla DATOS_USUARIO, se agrega un comentario con la accion y el id del usuario.
 DELIMITER //
 CREATE TRIGGER INSERCION_DATOS AFTER INSERT ON DATOS_USUARIO
 FOR EACH ROW
@@ -18,16 +19,18 @@ insert into REGISTROS_USUARIOS (movimiento) value (concat('Se cargaron los datos
 END // 
 DELIMITER ;
 
-
+// Trigger que luego de que captura una actualizacion de los datos de la tabla DATOS_USUARIO, registra un comentario
+// En la tabla REGISTROS_USUARIOS con la accion y el id del usuario
 DELIMITER //
 CREATE TRIGGER ACTUALIZACION_DATOS AFTER UPDATE ON DATOS_USUARIO
 FOR EACH ROW
 BEGIN
-insert into REGISTROS_USUARIOS (movimiento) values (concat('Los datos del usuario numero id: ', NEW.id_usuario, 'se modificaron'));
+insert into REGISTROS_USUARIOS (movimiento) values (concat('Los datos del usuario numero id: ', id_usuario, 'se modificaron'));
 END //
 DELIMITER ;
 
-
+// Trigger que registra una insercion en la tabla BANS e inserta un comentario con la accion
+// El id del usuario y del moderador en la tabla SANCIONES
 DELIMITER //
 CREATE TRIGGER SANCION AFTER INSERT ON BANS
 FOR EACH ROW
@@ -36,7 +39,8 @@ insert into SANCIONES (movimiento) values (concat('Se registro la sancion con nu
 END //
 DELIMITER ;
 
-
+// Trigger que registra una delete en la tabla BANS, inserta un comentario en la tabla SANCIONES
+// Con la accion, el id del usuario y del moderador
 DELIMITER //
 CREATE TRIGGER FINALIZACION_SANCION AFTER DELETE ON BANS
 FOR EACH ROW
